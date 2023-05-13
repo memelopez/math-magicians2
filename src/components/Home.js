2d33f289445c7b4c7f917c19a630adf5ec56ab6c
memelopez <meme30lopez@gmail.com>
2023-05-12 23:41:14 -0600

    Add Home page component

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <div className="subtitle">
+        <h2 className="actual-subt">Welcome to our page!</h2>
+      </div>
+      <div className="for-lorems">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer venenatis malesuada
+          semper. Proin et nunc viverra, vestibulum quam quis, consequat libero. Suspendisse
+          dapibus, risus in consequat convallis, ex odio tincidunt orci, in sagittis ipsum turpis
+          id diam. Aenean ultricies odio in mauris volutpat venenatis. Morbi pulvinar non odio
+          eget malesuada. Nam in hendrerit lacus, vulputate tincidunt neque. Mauris orci erat,
+          vestibulum fringilla vehicula vitae, imperdiet non lectus. Duis vel imperdiet sapien.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis odio earum illum pariatur
+          amet quisquam consequatur! Dicta iste praesentium quae modi adipisci. Voluptatum tempore
+          neque, commodi asperiores quia delectus excepturi.Pellentesque habitant morbi tristique
+          senectus et netus et malesuada fames ac turpis
+          egestas. Aliquam interdum imperdiet lorem, et pretium risus aliquet in. Mauris id
+          tellus justo.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

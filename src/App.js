112f3ef37f3864fabd9a323ee2259d9fd7d5dd26
memelopez <meme30lopez@gmail.com>
2023-05-12 23:43:14 -0600

    Add router configuration in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 // /src/app.js
 import './App.css';
-import Calculator from './components/Calculator';
-import QuoteComp from './components/QuoteComp';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
+import Home from './components/Home';
+import CalculatorPage from './components/CalculatorPage';
+import Quote from './components/Quote';
+import ErrorPage from './components/ErrorPage';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
-      <QuoteComp />
+      <Router>
+        <nav>
+          <h1 className="main-title">Math Magicians</h1>
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <span className="btw-links">|</span>
+            <Link to="/calc">Calculator</Link>
+            <span className="btw-links">|</span>
+            <Link to="/quote">Quote</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calc" element={<CalculatorPage />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
